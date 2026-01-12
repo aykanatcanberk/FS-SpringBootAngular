8a5e9f361c2b531d23ebf509830fc6ccf591fd73
Canberk Aykanat <kayra.aykanat@gmail.com>
2026-01-12 16:34:14 +0300

    repository

---
+++ Backend/src/main/java/com/movie/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.movie.app.repository;
+
+import com.movie.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ Backend/src/main/java/com/movie/app/repository/VideoRepository.java
@@ -0,0 +1,9 @@
+package com.movie.app.repository;
+
+import com.movie.app.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
+}
